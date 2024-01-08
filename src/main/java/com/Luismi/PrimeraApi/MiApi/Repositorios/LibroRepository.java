806956f3acb734ca3b975c66652b6ebe370b7a25
mainluismi <luismisc02@gmail.com>
2024-01-08 11:45:43 +0100

    Agregación funciones a la interfaz de libro

--- src/main/java/com/Luismi/PrimeraApi/MiApi/Repositorios/LibroRepository.java
+++ src/main/java/com/Luismi/PrimeraApi/MiApi/Repositorios/LibroRepository.java
@@ -4,6 +4,11 @@ import com.Luismi.PrimeraApi.MiApi.Entidades.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LibroRepository extends JpaRepository<Libro,Long> {
+    public Libro add(Libro libro);
+    public List<Libro> findAll();
+    public Libro findById(long id);
 }
